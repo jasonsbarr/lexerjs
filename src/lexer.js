6a8d09bec50f40a58fbdd0bfbd5d03874a9117cf
Jason Barr <jason.barr@metronetinc.com>
2022-03-03 18:44:27 -0600

    Create Token class

--- src/lexer.js
+++ src/lexer.js
@@ -3,3 +3,20 @@ class LexerError extends Error {
     super(`Invalid token ${char} at (${line}:${col})`);
   }
 }
+
+/**
+ * A Token represents a single lexeme
+ */
+class Token {
+  constructor(type, val, line, col, pos) {
+    this.type = type;
+    this.val = val;
+    this.line = line;
+    this.col = col;
+    this.pos = pos;
+  }
+
+  toString() {
+    return `Token(type=${this.type}, val=${this.val})`;
+  }
+}
